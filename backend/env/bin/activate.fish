a08cc7aa493a8dac9ef921ee2d797608ec4bfb88
tungleqb <tungleqb@gmail.com>
2025-03-22 15:19:19 +0000

    Mô tả thay đổi

--- backend/env/bin/activate.fish
+++ backend/env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/vboxuser/backend/env
+set -gx VIRTUAL_ENV /home/vboxuser/tramproxy2/backend/env
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
