258ccfdf13436f2bc6a9bc573e77d6a79cb98f17
tungleqb <tungleqb@gmail.com>
2025-03-24 11:31:56 +0700

    Lần 1

--- push-to-github.ps1
+++ push-to-github.ps1
@@ -5,6 +5,6 @@ $commitMsg = Read-Host "Nhập nội dung commit"
 git commit -m "$commitMsg"
 
 Write-Host "Đang push lên GitHub..."
-git push main
+git push -u origin main
 
 Write-Host "Đã cập nhật lên GitHub thành công!"
